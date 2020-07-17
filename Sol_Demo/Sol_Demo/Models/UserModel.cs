50af81ad46b9b1fc8a24e4fc9946ddd6e32cfaac
Kishor Naik <kishor.naik011.net@outlook.com>
2020-07-17 13:38:51 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Models/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Models
+{
+    public class UserModel
+    {
+        // https://docs.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-3.1
+        // Pre Defined Data Annotations
+        [Required]
+        [MaxLength(50, ErrorMessage = "First Name should be less than 50 character")]
+        [DisplayName("First Name")]
+        public String FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "First Name should be less than 50 character")]
+        [DisplayName("First Name")]
+        public String LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DisplayName("Email Id")]
+        public String EmilId { get; set; }
+    }
+}
